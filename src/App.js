2f0f5e56890a6edb239480bb16b299976c12d32a
ijs2000 <joshuvanani1999@gmail.com>
2023-07-05 19:21:43 -0400

    task32

--- src/App.js
+++ src/App.js
@@ -1,110 +1,36 @@
-import './App.css';
-import React from "react" ;
-import {Redirect,Route,Switch,} from 'react-router-dom'
-import Sidebar from './component/SideBar/sidbar';
-import Maindash from './component/maindash/maindash';
-import BtnDash from './component/Component dashboard/Buttons/button'
-import CardDash from './component/Component dashboard/Cards/card'
-import ColorDash from './component/Utilities Dashboard/Colors/color'
-import BorderDash from './component/Utilities Dashboard/Borders/border'
-import AnimationDash from './component/Utilities Dashboard/Animation/animation'
-import LoginDash from  './component/Pages dashboard/Login/login'
-import ForgotDash from './component/Pages dashboard/Forgot Password/forgot'
-import RegisterDash from './component/Pages dashboard/Register/register'
-import TableDash from './component/Tables Dashboard/table'
-import ErrorDash from './component/Pages dashboard/404 Page/404page'
-import Header from './component/Header component/header';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Navbar from "./components/layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
+import AddUser from "./components/users/AddUser";
+import EditUser from "./components/users/EditUser";
+import User from "./components/users/User";
 
-
-function App() {
- 
+function App(props) {
   return (
-  <div className='App'>
-     <div className='AppGlass'>
-
-           <Header/>
-      
-      
-          <Sidebar/>
-      
-      
-           <Body/>
-      
-      <div>
-
+    <Router>
+      <div className="App">
+        <h2>Book library</h2>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
-     </div>
-  </div>
+    </Router>
   );
 }
 
 export default App;
-                     
-
-const Body = () =>{
-  return(
-    <div className='body'>
-      <Switch>
-         <Route exact path='/dashboard'>
-         <Maindash/>
-         </Route>
-
-         <Route exact path='/'>
-          <Redirect to ='/dashboard'/>
-         </Route>
-
-         
-         <Route exact path='/component/cards'>
-          <CardDash/>
-         </Route>
-
-         <Route exact path='/component/button'>
-          <BtnDash/>
-         </Route>
-
-
-         <Route exact path='/utitlities/colors'>
-         <ColorDash/>
-         </Route>
-
-         <Route exact path='/utilities/borders'>
-          <BorderDash/>
-         </Route>
-
-         <Route exact path='/utilities/animations'>
-          <AnimationDash/>
-         </Route>
-
-         <Route exact path='/pages/login'>
-         <LoginDash/>
-         </Route>
-
-         <Route exact path='/pages/forgotpassword'>
-           <ForgotDash/>
-         </Route>
-
-         <Route exact path='/pages/register'>
-           <RegisterDash/>
-         </Route>
-
-         <Route exact path='/tables'>
-          <TableDash/>  
-         </Route>
-         
-         <Route exact path='**'>
-           <ErrorDash/>
-         </Route>
-
-      </Switch>
-
-      
-      
-    </div>
-  )
-}
-
-
-  
-
-
-  
